51c68e3e913aeb974a7b4bc80c0452d815c90678
RobertJing <jack.jing7@gmail.com>
2016-07-07 11:00:12 -0400

    modify the space

--- CalculatorKarma/karma.conf.js
+++ CalculatorKarma/karma.conf.js
@@ -7,11 +7,9 @@ module.exports = function(config) {
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
 
-
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['jasmine'],
-
 
         // list of files / patterns to load in the browser
         files: [
@@ -21,25 +19,20 @@ module.exports = function(config) {
             'tests/*.js'
         ],
 
-
         // list of files to exclude
         exclude: [],
-
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {},
-
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress'],
 
-
         // web server port
         port: 9876,
-
 
         // enable / disable colors in the output (reporters and logs)
         colors: true,
